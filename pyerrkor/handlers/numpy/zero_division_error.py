b48d35dfb3cc18d1a32114d18b95f827bb42640c
ehdcjf <kdch0823@naver.com>
2025-05-13 13:10:26 +0900

    라이브러리별 에러 분기

---
+++ pyerrkor/handlers/numpy/zero_division_error.py
@@ -0,0 +1,6 @@
+from pyerrkor.registry import register
+
+
+@register("ZeroDivisionError", source="numpy")
+def numpy_zero_division_error(exc_type, exc_value):
+    return "0 나누기 오류", "NumPy 연산 중 0으로 나누기를 시도했습니다."
